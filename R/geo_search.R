b012bed3e80275b40fb83c55c9e79e955f788f00
Matthew Mueller <MuellerMatthew@users.noreply.github.com>
2021-10-15 09:49:22 -0400

    Update geo_search.R

--- R/geo_search.R
+++ R/geo_search.R
@@ -4,7 +4,7 @@
 #' the Geoclient response as a tibble. The locations are provided either in a
 #' single vector as a named argument or with a dataframe and column name of the
 #' location field. This function is helpful when your address data is not
-#' separated into components. The Geoclient API's app ID and key can either be
+#' separated into components. The Geoclient API's key can either be
 #' provided directly as arguments, or you can first use [geoclient_api_keys()]
 #' to add them to your `.Renviron` file so they can be called securely without
 #' being stored in your code.
@@ -21,18 +21,18 @@
 #'   may have to make multiple requests to narrow in on the correct location.
 #'
 #'   For more details see the Geoclient Documentation's guide to [single-input
-#'   requests](https://api.cityofnewyork.us/geoclient/v1/doc#section-1.3),
+#'   requests](https://api.nyc.gov/geoclient/v1/doc#section-1.3),
 #'   interpreting the [Geosupport return
-#'   codes](https://api.cityofnewyork.us/geoclient/v1/doc#section-2.2), and a
+#'   codes](https://api.nyc.gov/geoclient/v1/doc#section-2.2), and a
 #'   [complete data
-#'   dictionary](https://api.cityofnewyork.us/geoclient/v1/doc#section-4.0) for
+#'   dictionary](https://api.nyc.gov/geoclient/v1/doc#section-4.0) for
 #'   all possible data elements returned by any geoclient function.
 #'
 #' @examples
 #'
 #' \dontrun{
 #'
-#' geoclient_api_keys("1a2b3c4", "9d8f7b6wh4jfgud67s89jfyw68vj38fh")
+#' geoclient_api_keys("9d8f7b6wh4jfgud67s89jfyw68vj38fh")
 #'
 #' geo_search(1005430053) # BBL
 #' geo_search("139 macdougal st mn") # Address
@@ -58,7 +58,7 @@
 #' @export
 geo_search_data <- function(.data, location, id = NULL, key = NULL, rate_limit = TRUE) {
 
-  creds <- get_creds(id, key)
+  creds <- get_creds(key)
 
   search_inputs <- validate_search_inputs(pull_or_null(.data, enquo(location)))
 
@@ -69,7 +69,7 @@ geo_search_data <- function(.data, location, id = NULL, key = NULL, rate_limit =
 #' @export
 geo_search <- function(location, id = NULL, key = NULL, rate_limit = TRUE) {
 
-  creds <- get_creds(id, key)
+  creds <- get_creds(key)
 
   search_inputs <- validate_search_inputs(location)
 
